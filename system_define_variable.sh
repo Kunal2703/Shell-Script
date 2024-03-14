c7ac15b8c37525f97ead7f277757fc7746477d27
Kunal <kunalsingh2703@gmail.com>
2024-03-15 00:51:28 +0530

    User define variable and System define variable

---
+++ system_define_variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ${SHELL}
+
+echo ${HOME}
+
+echo ${OSTYPE}
+
+echo ${$}	#process id
+echo ${PPID}	#parent process id
+
+echo $PWD
+echo $HOSTNAME
---
+++ user_define_variable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name="kunal"
+age="22"
+echo "My name is $name and my age is $age!!"
+
+#OR
+
+name="Julius"
+age="25"
+echo "My name is ${name} and my age is ${age}!!"
+
+#concate two variables
+
+work="Programm"
+var="ing"
+echo "I am ${work}${var}!!"
