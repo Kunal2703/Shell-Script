3ced0e156026eb0f996c87d2f37e7e622a97e519
Kunal2703 <kunalsingh2703@gmail.com>
2024-03-18 10:32:00 +0530

    take user input and command line argument

--- user_input.sh
+++ user_input.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-read name
-read age
 
-echo "My name is ${name} and my age is ${age}"
+# -p -> prompt
+read -p "Enter name: " name
+read -p "Enter age: " age
+
+# -s -> secure
+read -p "Enter password: " -s password 
+
+echo "My name is ${name} and my age is ${age} and my password is ${password}"
